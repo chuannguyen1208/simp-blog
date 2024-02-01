0969e42ef462f421a0ead3fb0ca7e1b4a6825ef9
chuannguyen1208 <trongchuannguyen@gmail.com>
2024-02-01 10:56:26 +0700

    service layer

---
+++ Simp.Blog.Service/Blogs/BlogClient.cs
@@ -0,0 +1,10 @@
+﻿namespace Simp.Blog.Service.Blogs;
+
+public interface IBlogClient
+{
+}
+
+internal class BlogClient
+{
+
+}
---
+++ Simp.Blog/Program.cs
@@ -0,0 +1,18 @@
+using Simp.Blog.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
