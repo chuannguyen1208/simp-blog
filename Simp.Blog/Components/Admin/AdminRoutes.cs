1ea35c004d1516ef7c9a9481d5db9eb634959270
chuannguyen1208 <trongchuannguyen@gmail.com>
2024-02-03 18:01:58 +0700

    update

---
+++ Simp.Blog/Components/Admin/AdminRoutes.cs
@@ -0,0 +1,12 @@
+﻿namespace Simp.Blog.Components.Admin;
+
+public static class AdminRoutes
+{
+    public const string Dashboard = "/admin";
+    public const string Blogs = "/admin/blogs";
+    public const string BlogsEdit = "/admin/blogs/{id:guid}";
+
+    public static string ToBlogsCreate() => $"{Blogs}/{Guid.Empty}";
+    public static string ToBlogsEdit(Guid Id) => $"{Blogs}/{Id}";
+
+}
---
+++ Simp.Blog/Components/User/UserRoutes.cs
@@ -0,0 +1,10 @@
+﻿namespace Simp.Blog.Components.User;
+
+public static class UserRoutes
+{
+    public const string Base = "/";
+    public const string Blogs = "/blogs";
+    public const string BlogsDetail = "/blogs/{id:guid}";
+
+    public static string ToBlogsDetail(Guid id) => $"/blogs/{id}";
+}
